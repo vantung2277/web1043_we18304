baf58927fdbc08b0e8a065c15b518e2f60c338a9
phan nguyen van tung <tungpnvpd08091v@fpt.edu.vn>
2023-03-13 17:43:41 +0700

    nop lab

---
+++ lab1/app.js
@@ -0,0 +1,14 @@
+// nhap so nguyen a va b
+var a = parseInt(prompt("Nhap so nguyen a "))
+var b = parseInt(prompt("Nhap so nguyen b "))
+var d = parseInt(prompt("Nhap so nguyen d "))
+// gan bien a b vao c
+var c = a + b
+// tinh tong c va d 
+var result = c + d 
+// xuat ra man hinh
+document.write(c)
+console.log(c);
+alert(result)
+console.log(result);
+kd
